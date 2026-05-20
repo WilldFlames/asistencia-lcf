2cb871a8d4c9e0f0066da248f94229df08a5c401
WilldFlames <zceciliano@gmail.com>
2026-05-19 20:21:59 -0600

    fix

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ const bcrypt = require("bcryptjs");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DATABASE_URL.includes("railway.internal")
+    ? false
+    : { rejectUnauthorized: false },
 });
 
 const MATERIAS_DEFAULT = [
